092c59f60d4b7a1ce4ce15bc76b94ba749a43f88
Shafayet098 <shafayethossenchowdhury@gmail.com>
2026-03-21 11:48:27 +0600

    complete implementation of post method

--- src/Pages/AddProperties/AddProperties.jsx
+++ src/Pages/AddProperties/AddProperties.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 
 const AddProperties = () => {
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form)
+        const realEstateData = Object.fromEntries(formData.entries());
+        console.log(realEstateData)
+        fetch('http://localhost:3000/addproperties', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(realEstateData)
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+
+    }
     return (
         <div className="card-body p-6 md:p-8 mt-16 container mx-auto">
             <h1 className="text-4xl font-bold text-center text-secondary">
@@ -11,7 +28,7 @@ const AddProperties = () => {
             </p>
 
             <div className='p-8 sm:p-12  bg-slate-100 shadow-lg rounded-md'>
-                <form className="space-y-5 bg-slate-100 text-xl">
+                <form onSubmit={handleFormSubmit} className="space-y-5 bg-slate-100 text-xl">
                     <div>
                         <label className="label mb-2">
                             <span className="text-secondary font-medium">Property Name</span>
@@ -31,7 +48,6 @@ const AddProperties = () => {
                         </label>
                         <textarea
                             name="description"
-
                             placeholder="Enter property description"
                             className="textarea textarea-bordered w-full h-28 outline-primary border-0 text-primary text-md font-semibold"
                             required
@@ -45,9 +61,7 @@ const AddProperties = () => {
                             </label>
                             <select
                                 name="category"
-
-                                className="select select-bordered w-full outline-primary border-0 text-primary text-md font-semibold"
-                            >
+                                className="select select-bordered w-full outline-primary border-0 text-primary text-md font-semibold"                    >
                                 <option>Rent</option>
                                 <option>Sale</option>
                                 <option>Commercial</option>
@@ -64,7 +78,6 @@ const AddProperties = () => {
                             <input
                                 type="number"
                                 name="price"
-
                                 placeholder="Enter price"
                                 className="input input-bordered w-full outline-primary border-0 text-primary text-md font-semibold"
                                 required
@@ -76,8 +89,7 @@ const AddProperties = () => {
                             </label>
                             <input
                                 type="text"
-                                name="price"
-
+                                name="squareFeet"
                                 placeholder="Enter Square Feet"
                                 className="input input-bordered w-full outline-primary border-0 text-primary text-md font-semibold"
                                 required
@@ -90,14 +102,12 @@ const AddProperties = () => {
                             <input
                                 type="number"
                                 name="beds"
-
                                 placeholder="Enter Beds Number"
                                 className="input input-bordered w-full outline-primary border-0 text-primary text-md font-semibold"
                                 required
                             />
                         </div>
                     </div>
-
                     <div>
                         <label className="label mb-2">
                             <span className="label-text font-medium text-secondary">Location</span>
@@ -105,7 +115,6 @@ const AddProperties = () => {
                         <input
                             type="text"
                             name="location"
-
                             placeholder="Enter city, area, or address"
                             className="input outline-primary border-0 w-full text-primary text-md font-semibold"
                             required
@@ -119,7 +128,6 @@ const AddProperties = () => {
                         <input
                             type="url"
                             name="imageLink"
-
                             placeholder="Enter image URL"
                             className="input w-full outline-primary border-0 text-primary text-md font-semibold"
                             required
@@ -134,7 +142,6 @@ const AddProperties = () => {
                             <input
                                 type="email"
                                 name="userEmail"
-
                                 placeholder="Enter your email"
                                 className="input outline-primary border-0 w-full text-primary text-md font-semibold"
                                 required
@@ -148,16 +155,15 @@ const AddProperties = () => {
                             <input
                                 type="text"
                                 name="userName"
-                                value=''
                                 placeholder="Enter your name"
-                                className="input outline-primary border-0 w-full text-primary text-md font-semibold"
+                                className="input outline-primary border-0 w-full  text-primary text-md font-semibold"
                                 required
                             />
                         </div>
                     </div>
 
                     <div className="pt-3">
-                        <button type="submit" className="btn border-2 border-primary  w-full text-xl   text-primary font-semibold hover:bg-primary hover:text-white">
+                        <button type="submit" className="btn border-2 border-primary  w-full text-xl  rounded-lg py-4  text-primary font-semibold hover:bg-primary hover:text-white">
                             Add Property
                         </button>
                     </div>
